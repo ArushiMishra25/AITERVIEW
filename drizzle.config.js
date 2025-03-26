4116db969bf963f9869df81b62ce205c2955716c
ArushiMishra25 <arushimishra2510@gmail.com>
2025-03-26 19:07:10 +0530

    removed the hardcoded env variables

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,8 +1,9 @@
+import "dotenv/config";
 /** @type { import("drizzle-kit").Config } */
 export default {
   schema: "./utils/schema.js",
   dialect: "postgresql",
   dbCredentials: {
-    url: "postgresql://neondb_owner:npg_D0wQ4IsfhWtr@ep-icy-shape-a5siu2sw-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require",
+    url: process.env.NEXT_PUBLIC_DRIZZLE_DATABASE_URL,
   },
 };
